0092575117071f746d2c3776ea10dff6b3040bfb
Jean Pierre Veliz Lavayen <jeanpierreveliz26@gmail.com>
2026-06-11 17:45:51 -0500

    understansding architeticture import export

--- src/Components/p5_funtions/WhiteBoard.tsx
+++ src/Components/p5_funtions/WhiteBoard.tsx
@@ -4,17 +4,17 @@ export const whiteboard = (p: p5) => {
 
     p.setup = () => {
         p.createCanvas(p.windowWidth, p.windowHeight)
-        p.strokeWeight(3)
-        p.background(0, 0, 0)
-        p.colorMode(p.HSB);
+        p.strokeWeight(5)
+        p.background(255, 255, 255)
+        p.colorMode(p.RGB);
         p.describe('A blank canvas where the user draws by dragging the mouse')
         p.filter(p.BLUR, 10)
-        p.fill(0,150,100)
+        p.fill(255,0,0)
         p.rect(100,100,200,200)
 
         p.mouseDragged = () => {
             //let lineHue = p.mouseX - p.mouseY;
-            p.stroke(0, 0, 100);
+            p.stroke(0, 0, 0);
             p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY);
         }
     }   
